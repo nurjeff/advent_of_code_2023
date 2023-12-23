36056366b15c43a9020eb367efaa80ed56acdd78
nurjeff <jeffrey.schmitz@gmx.de>
2023-12-23 17:43:24 +0100

    clean

--- day6/main.go
+++ day6/main.go
@@ -6,21 +6,9 @@ import (
 	"os"
 	"strconv"
 	"strings"
-	"time"
 )
 
 func main() {
-	parseInput()
-}
-
-type Set struct {
-	Time     int
-	Distance int
-}
-
-var sets []Set
-
-func parseInput() {
 	file, err := os.Open("input_2.txt")
 	if err != nil {
 		panic(err)
@@ -28,10 +16,7 @@ func parseInput() {
 	defer file.Close()
 
 	input, _ := io.ReadAll(file)
-	inputString := string(input)
-
-	lines := strings.Split(strings.ReplaceAll(inputString, "\r\n", "\n"), "\n")
-
+	lines := strings.Split(strings.ReplaceAll(string(input), "\r\n", "\n"), "\n")
 	times := strings.Split(lines[0], " ")
 	distances := strings.Split(lines[1], " ")
 
@@ -41,14 +26,12 @@ func parseInput() {
 		sets = append(sets, Set{Time: int(t), Distance: int(d)})
 	}
 
-	t := time.Now()
 	answer := 1
 	for _, e := range sets {
 		winsamounts := 0
 		for i := 0; i <= e.Time; i++ {
 			holdingTime := e.Time - i
-			remainingTime := e.Time - holdingTime
-			distance := holdingTime * remainingTime
+			distance := holdingTime * (e.Time - holdingTime)
 			if distance > e.Distance {
 				winsamounts++
 			}
@@ -56,5 +39,12 @@ func parseInput() {
 		answer *= winsamounts
 
 	}
-	fmt.Println(answer, time.Since(t))
+	fmt.Println(answer)
 }
+
+type Set struct {
+	Time     int
+	Distance int
+}
+
+var sets []Set
